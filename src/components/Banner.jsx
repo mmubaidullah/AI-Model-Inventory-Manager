0ad19828ddaeaed1ae3510e98ced061fe09bfbef
mmubaidullah <ubaid111999@gmail.com>
2025-11-24 00:37:50 +0600

    Updete banner filesr

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+// import bannerLogo from "../assets/AI_model.webp"
+import MotionSlider from "./MotionSlider/MotionSlider";
 
 const Banner = () => {
   return (
     <div>
-      
+      {/* <img className='w-full mx-auto ' src={bannerLogo} alt="" /> */}
+      <div className="text-center px-8 flex flex-col mt-12  leading-relaxed">
+        <h1 className=" text-center font-bold lg:text-4xl md:text-3xl text-2xl mb-2 leading-relaxed heading-text-dark-aware">
+          Talk to Tomorrow’s Technology
+        </h1>
+        <p className="leading-relaxed mb-12 text-gray-500">
+          Experience the future of seamless communication with AI{" "}
+        </p>
+      </div>
+      <MotionSlider></MotionSlider>
     </div>
   );
 };
